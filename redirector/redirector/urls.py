81c5dae981d2807b69a6e3847f75fa94c627c289
DED8IRD <ewu72829@gmail.com>
2018-08-17 13:33:58 -0700

    refactor

--- redirector/redirector/urls.py
+++ redirector/redirector/urls.py
@@ -1,4 +1,4 @@
-"""URLshortenersite URL Configuration
+"""redirector URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from URLshortenersite import views
+from redirector import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- redirector/redirector/wsgi.py
+++ redirector/redirector/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for URLshortenersite project.
+WSGI config for redirector project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "URLshortenersite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redirector.settings")
 
 application = get_wsgi_application()
